f122676d84d311d2a1a0bd3f464c81b4f1a56476
Ghos13 <robocopsoo095@gmail.com>
2025-09-25 18:18:15 +0600

    add adaptiv

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,52 +1,72 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../src/images/DTS-logobr.png";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
-      <header className="header">
-        <div className="container">
-          <div className="header-menu">
-            <div className="header-menu_logo">
-              <Link to="/">
-                {/* <img className="header_logo" src={logo} alt="logo-DTS" /> */}
-                <i>
-                  <strong className="header-menu_strong">DTS</strong>{" "}
-                </i>
-              </Link>
-            </div>
-            <nav className="header-menu_nav">
-              <ul className="header-navbar">
-                <li>
-                  <Link className="link-in-header" to="/catalog">каталог</Link>
-                </li>
-                <li>
-                  <Link className="link-in-header" to="/cart">себет</Link>
-                </li>
-                {/* <li>
-                  <Link className="link-in-header" to="/checkout">төлөм</Link>
-                </li> */}
-                {/* <li>
-                  <Link className="link-in-header" to="/profile">профиль</Link>
-                </li> */}
-                <li>
-                  <Link className="link-in-header" to="/about">биз тууралуу</Link>
-                </li>
-                <li>
-                  <Link className="link-in-header" to="/contacts">контакттар</Link>
-                </li>
-                <li>
-                  <Link className="link-in-header" to="/news">жаңылыктар</Link>
-                </li>
-                <li>
-                  <Link className="link-in-header" to="/events">иш-чаралар</Link>
-                </li>
-              </ul>
-              <p></p>
-            </nav>
+    <header className="header">
+      <div className="container">
+        <div className="header-menu">
+          {/* ЛОГО */}
+          <div className="header-menu_logo">
+            <Link to="/">
+              {/* <img className="header_logo" src={logo} alt="logo-DTS" /> */}
+              <i>
+                <strong className="header-menu_strong">DTS</strong>
+              </i>
+            </Link>
           </div>
+
+          {/* БУРГЕР */}
+          <button
+            className={`burger ${isOpen ? "active" : ""}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+
+          {/* МЕНЮ */}
+          <nav className={`header-menu_nav ${isOpen ? "open" : ""}`}>
+            <ul className="header-navbar">
+              <li>
+                <Link className="link-in-header" to="/catalog">
+                  каталог
+                </Link>
+              </li>
+              <li>
+                <Link className="link-in-header" to="/cart">
+                  себет
+                </Link>
+              </li>
+              <li>
+                <Link className="link-in-header" to="/about">
+                  биз тууралуу
+                </Link>
+              </li>
+              <li>
+                <Link className="link-in-header" to="/contacts">
+                  контакттар
+                </Link>
+              </li>
+              <li>
+                <Link className="link-in-header" to="/news">
+                  жаңылыктар
+                </Link>
+              </li>
+              <li>
+                <Link className="link-in-header" to="/events">
+                  иш-чаралар
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
 
